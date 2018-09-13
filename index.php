871d74f8df639dd28bf6c25512a689df2f1d6e0f
Francisco Hernandez <frhernandez@csumb.edu>
2018-09-13 00:55:35 +0000

    Labs & Homework added

--- index.php
+++ index.php
@@ -5,8 +5,16 @@
 // then click the URL that is emitted to the Output tab of the console
 
 echo "<h1> Francisco Hernandez CST 336 site</h1>";
+echo "<br />";
 
-echo "Test";
+echo '<a href="/cst336/labs/lab1/h.html"> Lab 1 </a>';
+echo "<br />";
+
+echo '<a href="/cst336/labs/lab2/index.php"> Lab 2 </a>';
+echo "<br />";
+
+echo '<a href="/cst336/homework/hw1/home.html"> Homework 1 </a>';
+echo "<br />";
 
 ?>
 </body>
