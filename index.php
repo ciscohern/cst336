d5e5961d4eb464d9723c31ea27b5c74aae168415
Francisco Hernandez <frhernandez@csumb.edu>
2018-11-01 02:22:28 +0000

    Lab 6

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ echo '<a href="/labs/lab4/index.php"> Lab 4 </a>';
 echo "<br />";
 echo '<a href="/labs/lab5/scart/index.php"> Lab 5 </a>';
 echo "<br />";
-echo '<a href="/labs/lab4/index.php"> Lab 6 </a>';
+echo '<a href="/labs/lab6/index.php"> Lab 6 </a>';
 echo "<br />";
 echo '<a href="/homework/hw1/home.html"> Homework 1 </a>';
 echo "<br />";
