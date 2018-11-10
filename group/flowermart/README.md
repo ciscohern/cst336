052c760058ca6db00490beac002c4da3aee0d760
Francisco Hernandez <frhernandez@csumb.edu>
2018-11-10 02:29:51 +0000

    Lab 7 index

--- group/flowermart/README.md
+++
@@ -1,2 +0,0 @@
-# flowermartcst336
-# test
--- index.php
+++ index.php
@@ -16,6 +16,8 @@ echo '<a href="/labs/lab5/scart/index.php"> Lab 5 </a>';
 echo "<br />";
 echo '<a href="/labs/lab6/index.php"> Lab 6 </a>';
 echo "<br />";
+echo '<a href="/labs/lab7/index.php"> Lab 7 </a>';
+echo "<br />";
 echo '<a href="/homework/hw1/home.html"> Homework 1 </a>';
 echo "<br />";
 echo '<a href="/homework/hw2/index.php"> Homework 2 </a>';
